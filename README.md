0efd3b9f03f969f1f45c63cb3598eab0f5812537
Naomi Alterman <naomila@uw.edu>
2022-09-12 11:16:41 -0700

    Auto-locate subl.exe

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Transparent bridge to interact with a Windows installation of Sublime Text from 
 
 [wslpy](https://wslutiliti.es/wslpy/)
 
-## Installation
+## Installation and configuration
 
 From within WSL:
 
@@ -14,6 +14,8 @@ From within WSL:
 cd /usr/local/bin
 ln -s {REPO DIR}/subl.py subl
 ```
+
+The script searches for the Windows installation of Sublime Text in `/mnt/c/Program Files` and `/mnt/c/Program Files (x86)`. If Sublime is installed elsewhere, you can explicitly point it to `subl.exe` with the `SUBLIME_PATH` environment variable.
 
 ## Usage
 
@@ -23,6 +25,3 @@ Use as you normally would invoke `subl` from a terminal:
 
 Paths are automatically converted from WSL to Windows syntax via [wslpy](https://wslutiliti.es/wslpy/).
 
-## TODO
-
-- Currently, the script expects Sublime to be installed to `"C:\Program Files\Sublime Text\"`. This should either be parameterizable via environment variable or, preferably, auto-detected.
--- subl.py
+++ subl.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 import sys
 import os
+import glob
 
 import wslpy.convert
 import wslpy.exec
+import wslpy.system
 
-# TODO: auto-detect?
-SUBLIME_PATH = "C:\\Program Files\\Sublime Text\\subl.exe"
+def findWinSubl():
+    program_dirs = ["/mnt/c/Program Files", "/mnt/c/Program Files (x86)"]
+    possible_subl_dirs = []
+    for program_dir in program_dirs:
+        possible_subl_dirs += glob.glob(os.path.join(program_dir, "*ublime*"))
+    for possible_subl_dir in possible_subl_dirs:
+        subl_path = os.path.join(possible_subl_dir, "subl.exe")
+        if os.path.isfile(subl_path):
+            return subl_path
+    raise Exception("Could not find Sublime Text installation")
+
+SUBLIME_PATH = os.environ.get("SUBLIME_PATH", findWinSubl())
 
 def canonicalize(path):
     return os.path.realpath(os.path.expanduser(path))
@@ -21,7 +33,7 @@ def join(args):
     return " ".join(quote(arg) for arg in args)
 
 if __name__=="__main__":
-    win_args = [SUBLIME_PATH]
+    win_args = [convert(SUBLIME_PATH)]
     suppress_conversion = False
     always_convert = False
     for arg in sys.argv[1:]:
